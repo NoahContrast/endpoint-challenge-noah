89c1c6e3ca3887b4ab6aff96c6a128cd37a11e5c
Mark Weeks <marrkkuu@gmail.com>
2020-08-20 16:26:36 +0100

    Adding spring intitializr base line spring boot project

---
+++ src/test/java/com/contrast/endpointchallenge/EndpointChallengeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.contrast.endpointchallenge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EndpointChallengeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
