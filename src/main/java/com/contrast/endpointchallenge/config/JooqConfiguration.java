18dc83b5e11e027c840a57fb1f1ec60ea245b24b
Mark Weeks <marrkkuu@gmail.com>
2020-08-21 10:43:12 +0100

    Adding jooq, jooq codegen, simpleflatmapper to map results sets with one to many relationships

---
+++ src/main/java/com/contrast/endpointchallenge/config/JooqConfiguration.java
@@ -0,0 +1,43 @@
+package com.endpoint.endpoint.config;
+
+import org.jooq.impl.DataSourceConnectionProvider;
+import org.jooq.impl.DefaultConfiguration;
+import org.jooq.impl.DefaultDSLContext;
+import org.jooq.impl.DefaultExecuteListenerProvider;
+import org.springframework.boot.autoconfigure.jooq.JooqExceptionTranslator;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.datasource.TransactionAwareDataSourceProxy;
+
+import javax.sql.DataSource;
+
+@Configuration
+public class JooqConfiguration {
+
+    private final DataSource dataSource;
+
+    public JooqConfiguration(final DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
+
+    @Bean
+    public DataSourceConnectionProvider connectionProvider() {
+        return new DataSourceConnectionProvider
+                (new TransactionAwareDataSourceProxy(dataSource));
+    }
+
+    @Bean
+    public DefaultDSLContext dsl() {
+        return new DefaultDSLContext(configuration());
+    }
+
+    public DefaultConfiguration configuration() {
+        DefaultConfiguration jooqConfiguration = new DefaultConfiguration();
+        jooqConfiguration.set(connectionProvider());
+        jooqConfiguration
+                .set(new DefaultExecuteListenerProvider(new JooqExceptionTranslator()));
+
+        return jooqConfiguration;
+    }
+
+}
